eacc346f9e004d37c0e87de11907ea82f453ef0e
BartoszTurczynski <bart.turczynski@gmail.com>
2024-04-05 08:31:10 +0200

    implementacja funkcji

--- sortowanie.cs
+++ sortowanie.cs
@@ -5,6 +5,28 @@ class JumpSearchExample
 {
     static int JumpSearch(List<int> arr, int x)
     {
+        int n = arr.Count;
+        int step = (int)Math.Floor(Math.Sqrt(n));
+
+        int prev = 0;
+        while (arr[Math.Min(step, n) - 1] < x)
+        {
+            prev = step;
+            step += (int)Math.Floor(Math.Sqrt(n));
+            if (prev >= n)
+                return -1;
+        }
+
+        while (arr[prev] < x)
+        {
+            prev++;
+            if (prev == Math.Min(step, n))
+                return -1;
+        }
+
+        if (arr[prev] == x)
+            return prev;
+
         return -1;
     }
 
