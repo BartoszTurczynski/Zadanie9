f4a1236a1cfd95b6581a5db70c8b8bb16390bcaf
BartoszTurczynski <bart.turczynski@gmail.com>
2024-04-05 08:29:55 +0200

    dodanie funkcji JumpSearch

--- sortowanie.cs
+++ sortowanie.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 
 class JumpSearchExample
 {
+    static int JumpSearch(List<int> arr, int x)
+    {
+        return -1;
+    }
+
     static void Main(string[] args)
     {
         List<int> arr = new List<int> { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
         int x = 13;
-        int index = -1;
+        int index = JumpSearch(arr, x);
 
         if (index != -1)
             Console.WriteLine("Element " + x + " found at index " + index);
